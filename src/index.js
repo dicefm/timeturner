2870ea9b5796323335015e4a20be2f8e81c87c3a
Alexander Johansson <alexander@n1s.se>
2015-08-27 17:56:04 +0200

    :sparkles: kue init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-export default function() {
-    return true;
+import _ from 'lodash';
+import kue from 'kue';
+
+export default function(opts) {
+    opts = _.defaultsDeep({
+        kue: {
+            prefix: 'q',
+            redis : {
+                host   : '127.0.0.1',
+                port   : 6379,
+                auth   : '',
+                options: {
+                    // see https://github.com/mranney/node_redis#rediscreateclient
+                }
+            }
+        },
+        concurrency: 5,
+    }, opts);
+
+    const queue = kue.createQueue(opts.kue);
+
+    queue.process('request', opts.concurrency, async function(job, done) {
+        setTimeout( () => done(), 1000);
+    });
+
+    return {
+        queue: queue,
+        kue  : kue,
+    };
 }
