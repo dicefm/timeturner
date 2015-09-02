4de0536d17881688b675b1339292dcaf25bc5de1
Alexander Johansson <alexander@n1s.se>
2015-09-02 12:16:38 +0100

    :sparkles: perform HTTP requests

--- src/lib/request-processor.js
+++ src/lib/request-processor.js
@@ -1,15 +1,33 @@
 import _ from 'lodash';
+import request from 'request-promise';
+import HTMLEntities from 'he';
 
-export default function() {
+const debug = require('debug')('dice:timeturner:request-processor');
+
+export default function(opts) {
     let i = 0;
     return async function(job, done) {
-        i++;
-        setTimeout(function() {
-            if (i % 2) {
-                done(new Error('Something went wrong!'));
-            } else {
-                done();
-            }
-        }, _.random(1000, 5000));
+        try {
+            const {data} = job;
+
+            const {url, headers} = data;
+
+            const reqOpts = {
+                uri    : url,
+                headers: headers,
+
+                resolveWithFullResponse: true,
+            };
+
+            debug('performing request with reqOpts:', reqOpts);
+
+            const response = await request(reqOpts);
+
+            done(null, response);
+        } catch (err) {
+            debug(`ERROR ${err.statusCode} ${err.name}`);
+            err.message = HTMLEntities.encode(err.message);
+            done(err);
+        }
     };
 }
