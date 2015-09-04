38de6682375c44dca6565f8d97c304aa9c03f71f
Alexander Johansson <alexander@n1s.se>
2015-09-04 15:39:49 +0100

    :white_check_mark: some more testing

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,13 +1,6 @@
 import index from './index';
 
 describe('index', () => {
-    let tt;
-    it('should work', () => {
-        tt = index({autoStart: false});
-        expect(tt).to.be.ok;
-    });
-
-    afterEach((done) => {
-        tt.queue.shutdown(done);
+    it('should have tests', () => {
     });
 });
--- src/lib/queue.spec.js
+++ src/lib/queue.spec.js
@@ -2,6 +2,8 @@ import _ from 'lodash';
 import queueModule from './queue';
 
 describe('queueModule', () => {
+    let _queue;
+
     function queueFactory(opts) {
         let {kue, processRequest, concurrency, apiClient} = opts;
         if (!apiClient) {
@@ -31,6 +33,7 @@ describe('queueModule', () => {
 
     it('should run jobs successfully', (done) => {
         const {queue, kue, processRequest, concurrency, apiClient} = queueFactory({});
+        _queue = queue;
 
         const _id = '' + Math.random();
         const data = {error: false, _id: _id};
@@ -55,6 +58,44 @@ describe('queueModule', () => {
                 done();
             });
         });
-        job.on('job failed', done);
+        job.on('failed', done);
+        queue.on('error', done);
+    });
+
+    it('should fail jobs sometimes too', (done) => {
+        const {queue, kue, processRequest, concurrency, apiClient} = queueFactory({});
+        _queue = queue;
+
+        const _id = '' + Math.random();
+        const data = {error: true, _id: _id};
+        const job = queue
+            .create('request', data)
+            .save((err) => {
+                if (err) {
+                    done(err);
+                }
+            })
+            ;
+
+        job.on('complete', () => {
+            done(new Error('Shouldn\'t have succeeded!'));
+        });
+        job.on('failed', () => {
+            _.defer(() => {
+                expect(processRequest).to.have.been.calledOnce;
+                expect(processRequest.args[0][0].id).to.eq(''+job.id);
+                expect(processRequest.args[0][0].data).to.deep.eq(data);
+
+                expect(apiClient.setState).to.have.been.calledOnce;
+                expect(apiClient.setState).to.have.been.calledWith(_id, 'FAIL');
+
+                done();
+            });
+        });
+        queue.on('error', done);
+    });
+
+    afterEach((done) => {
+        _queue.shutdown(done);
     });
 });
