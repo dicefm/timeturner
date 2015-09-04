25777eee8dc87f2a742dd74152453aa6fa329eb8
Alexander Johansson <alexander@n1s.se>
2015-09-04 10:55:30 +0100

    :art: middleware dep on tt instead of otherway around

--- examples/express/server.js
+++ examples/express/server.js
@@ -5,9 +5,12 @@ import morgan from 'morgan';
 
 import bodyParser from 'body-parser';
 
+import timeturner from '../../src/index';
 import timeturnerMiddleware from '../../src/middleware/express';
 
 const debug = require('debug')('dice:timeturner:express');
+
+const {PORT, MONGO_URL, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD} = process.env;
 
 const server = express();
 server.use(morgan('combined'));
@@ -15,12 +18,33 @@ server.set('state namespace', 'App');
 
 server.use(bodyParser.json());
 
+let opts = {};
 
-server.use('/schedule', timeturnerMiddleware());
+if (MONGO_URL) {
+    opts.mongodb = {url: MONGO_URL};
+}
+
+opts.kue = {
+    redis: {}
+};
+if (REDIS_HOST) {
+    opts.kue.redis.host = REDIS_HOST;
+}
+if (REDIS_PORT) {
+    opts.kue.redis.port = REDIS_PORT;
+}
+if (REDIS_PASSWORD) {
+    opts.kue.redis.password = REDIS_PASSWORD;
+}
 
 
 
-const port = process.env.PORT || 2015;
+const tt = timeturner(opts);
+server.use('/schedule', tt.expressMiddleware());
+
+
+
+const port = PORT || 2015;
 server.listen(port);
 debug('Listening on port ' + port);
 
