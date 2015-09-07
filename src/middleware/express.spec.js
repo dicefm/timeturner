92c673f59d9ae1c40fdcf303c00dea80f0c2194f
Alexander Johansson <alexander@n1s.se>
2015-09-07 15:40:10 +0100

    :white_check_mark: more middleware tests

--- src/middleware/express.spec.js
+++ src/middleware/express.spec.js
@@ -8,6 +8,10 @@ describe('expressMiddleware', () => {
     let tt;
     let req;
 
+    const uniqueUrl = `https://test.dice.fm/?${Math.random()}`;
+    const uniqueUrl2 = `https://test.dice.fm/?${Math.random()}`;
+    let createdItem;
+
     beforeEach(() => {
         tt = timeturner({autoStart: false});
 
@@ -26,4 +30,77 @@ describe('expressMiddleware', () => {
         expect(res.statusCode).to.eq(200);
         expect(res.body).to.be.an('array');
     }));
+
+    describe(`when creating an item with a unique URL`, () => {
+        const date = new Date();
+
+        let id;
+
+        let entry;
+
+        it('should create it successfully', asyncWithCallback(async () => {
+            const date = new Date();
+            const {body, statusCode} = await req.post('/schedule').send({
+                method: 'GET',
+                date  : date,
+                url   : uniqueUrl,
+            });
+
+            expect(statusCode).to.eq(200);
+            expect(body).to.be.an('object');
+
+            expect(body._id).to.be.a('string');
+
+            entry = body;
+            id = body._id;
+        }));
+
+        it('should find it by /:id', asyncWithCallback(async () => {
+            const date = new Date();
+            const {body, statusCode} = await req.get(`/schedule/${id}`);
+
+            expect(statusCode).to.eq(200);
+            expect(body).to.be.an('object');
+            expect(body).to.deep.eq(entry);
+        }));
+
+        it('should find it by /?url=:url', asyncWithCallback(async () => {
+            const date = new Date();
+            const {body, statusCode} = await req.get(`/schedule/?url=${entry.url}`);
+
+            expect(statusCode).to.eq(200);
+            expect(body).to.be.an('array');
+            expect(body).to.deep.eq([entry]);
+        }));
+
+        describe('and trying to update it', () => {
+            it('should be able to update it', asyncWithCallback(async () => {
+                const date = new Date();
+                const {body, statusCode} = await req.patch(`/schedule/${id}`, {name: uniqueUrl2});
+
+                expect(statusCode).to.eq(200);
+
+                entry.url = uniqueUrl2;
+
+                expect(body).to.deep.eq(body);
+            }));
+        });
+
+        describe('and trying to delete it', () => {
+            it('should be deleted', asyncWithCallback(async () => {
+                const date = new Date();
+                const {body, statusCode} = await req.delete(`/schedule/${id}`);
+
+                expect(statusCode).to.eq(204);
+            }));
+
+            it('and not found again', asyncWithCallback(async () => {
+                const date = new Date();
+                const {body, statusCode} = await req.get(`/schedule/${id}`);
+
+                expect(statusCode).to.eq(404);
+            }));
+        })
+    });
+
 });
