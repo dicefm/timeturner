9dcdce4344b01adfc48fe0429e8fa24486fef70b
Alexander Johansson <alexander@n1s.se>
2015-09-07 15:00:25 +0100

    :white_check_mark: test middleware

--- src/middleware/express.spec.js
+++ src/middleware/express.spec.js
@@ -1,6 +1,29 @@
-import expressMiddleware from './express';
+import timeturner from '../';
+
+
+import express from 'express';
+import superTestAsPromised from 'supertest-as-promised';
 
 describe('expressMiddleware', () => {
-    it('should have tests', () => {
+    let tt;
+    let req;
+
+    beforeEach(() => {
+        tt = timeturner({autoStart: false});
+
+        const {PORT} = process.env;
+
+        const server = express();
+
+        server.use('/schedule', tt.expressMiddleware());
+
+        req = superTestAsPromised(server);
     });
+
+    it('should GET /', asyncWithCallback(async () => {
+        const res = await req.get('/schedule');
+
+        expect(res.statusCode).to.eq(200);
+        expect(res.body).to.be.an('array');
+    }));
 });
