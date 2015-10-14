6d15656770598f998e26ff871c803b6b8f787e07
Alexander Johansson <alexander@n1s.se>
2015-10-14 12:51:23 +0100

    :sparkles: queue event emitter

--- src/lib/queue.js
+++ src/lib/queue.js
@@ -1,6 +1,6 @@
 import async from 'async';
 import _ from 'lodash';
-import Promise from 'bluebird';
+import {EventEmitter} from 'events';
 
 const debug = require('debug')('dice:timeturner:queue');
 
@@ -10,26 +10,36 @@ export default function(opts) {
 
     const {processJob, concurrency, apiClient, jobName} = opts;
 
+    const events = new EventEmitter();
+
     async function worker(job, callback) {
         debug('working on job', job);
         let _id;
         let error = null;
         try {
+            events.emit('job:init', {job});
+
             _id = job._id;
             await processJob(job);
+            events.emit('job:success', {job});
         } catch (_error) {
             debug('job failed', _error);
             error = _error;
+            events.emit('job:fail', {job, error});
         }
 
         const state = (error ? 'FAIL' : 'SUCCESS');
 
         try {
-            await apiClient.setState(_id, {state, error});
+            const newState = {state, error};
+            events.emit('job:set-state:init', {job, newState});
+            await apiClient.setState(_id, newState);
+            events.emit('job:set-state:success', {job, newState});
         } catch (_error) {
             debug('setState failed', _error);
 
             error = _error;
+            events.emit('job:set-state:fail', {job, newState});
         }
 
         callback(error);
@@ -38,6 +48,10 @@ export default function(opts) {
     // init queue
     const queue = async.queue(worker, concurrency);
 
+    const {push} = queue;
 
-    return queue;
+    return {
+        push,
+        events,
+    };
 };
