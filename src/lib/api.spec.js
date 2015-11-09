d600b639db23f9bfc2110748962c47379cbd5961
Ty <ty@dice.fm>
2015-11-09 18:16:40 +0000

    :white_check_mark: apiClient.setState

--- src/lib/api.spec.js
+++ src/lib/api.spec.js
@@ -1,6 +1,31 @@
-import api from './api';
+import timeturner from '../index';
 
-describe('api', () => {
-    it('should have tests', () => {
+const debug = require('debug')('dice:timeturner:api:spec');
+
+describe('api', async () => {
+    let tt;
+    before(() => {
+        tt = timeturner({
+            mongodb: {
+                url: 'mongodb://localhost:27017/timeturner_index_tests',
+            },
+        });
+    });
+
+    after(() => {
+        tt.loop.stop();
+    });
+
+    it('should setState', async () => {
+        const {RequestModel} = tt;
+        const request = await tt.apiClient.create({
+            url   : 'https://dice.fm/test',
+            method: 'GET',
+            date  : new Date(),
+        });
+        expect(request.state).to.eq('SCHEDULED');
+        await tt.apiClient.setState(request._id, {state: 'FAIL'});
+        const updated = await tt.apiClient.readId(request._id);
+        expect(updated.state).to.eq('FAIL');
     });
 });
