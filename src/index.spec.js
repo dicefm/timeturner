c987243acfac538072a9df0bdb6ad3cd0a65d153
KATT <alexander@n1s.se>
2016-02-16 15:50:59 -0200

    :white_check_mark: better integration test
    #3

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,6 +1,8 @@
 import timeturner from './index';
 
+import nock from 'nock';
 import superTestAsPromised from 'supertest-as-promised';
+
 import express from 'express';
 import bodyParser from 'body-parser';
 
@@ -70,7 +72,10 @@ describe('timeturner', () => {
                 mongodb: {
                     url: 'mongodb://localhost:27017/timeturner_integration_tests',
                 },
+                interval: 10,
             });
+
+            await tt.RequestModel.remove();
 
 
             server.use('/schedule', tt.expressMiddleware());
@@ -107,6 +112,59 @@ describe('timeturner', () => {
             expect(res.body).to.be.an('array');
 
             expect(res.body.length).to.eq(0);
+        });
+
+        describe('creating request', () => {
+            const TARGET_URL = 'https://api-test.dice.fm';
+
+            let targetCalledSpy;
+
+            before(() => {
+                targetCalledSpy = sinon.spy();
+
+                nock('https://api-test.dice.fm')
+                    .get('/')
+                    .reply(200, targetCalledSpy);
+            });
+
+            it('should create the request in 2ms', async () => {
+                const soon = new Date();
+                soon.setMilliseconds(soon.getMilliseconds() + 2);
+
+                const reqBody = {
+                    url   : TARGET_URL,
+                    date  : soon,
+                    method: 'GET',
+                };
+                const {body, statusCode} = await req.post('/schedule').send(reqBody);
+                expect(statusCode).to.eq(200);
+
+                expect(body._id).to.be.truthy;
+                expect(targetCalledSpy).to.have.been.notCalled;
+            });
+
+            it('should find the request in `/`', async () => {
+                const res = await req.get('/schedule');
+
+                expect(res.statusCode).to.eq(200);
+                expect(res.body).to.be.an('array');
+
+                expect(res.body.length).to.eq(1);
+            });
+
+            it('should trigger the request after a while', (next) => {
+                function checkCalled() {
+                    if (targetCalledSpy.notCalled) {
+                        setTimeout(checkCalled, 10);
+                    } else {
+                        expect(targetCalledSpy).to.have.been.calledOnce;
+
+                        next();
+                    }
+                }
+
+                checkCalled();
+            });
         });
     });
 });
